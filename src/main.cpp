9d8cdab03152e11d36f1493917aa254a1b0e26aa
lbo <lbo@spheniscida.de>
2013-05-19 13:06:43 +0200

    New indentation; removed memleak

--- src/main.cpp
+++ src/main.cpp
@@ -25,26 +25,26 @@ const char* ht_body_filename = 0;
 void setup_httpdata(void)
 {
     string header =
-"HTTP/1.1 503 Service Unavailable\x0d\x0a"
-"Server: 503srv\x0d\x0a"
-"Connection: close\x0d\x0a"
-"Content-Length: ";
+	"HTTP/1.1 503 Service Unavailable\x0d\x0a"
+	"Server: 503srv\x0d\x0a"
+	"Connection: close\x0d\x0a"
+	"Content-Length: ";
 
     string body;
 
     if ( ht_body_filename == 0 )
     {
 	body =
-"<html>\n"
-"    <head>\n"
-"	<title>503 Service Unavailable</title>\n"
-"    </head>\n"
-"   <body>\n"
-"	<h1>503 Service Temporarily Unavailable</h1>\n"
-"	    This HTTP service is unfortunately temporarily unavailable :(\n"
-"	    Maybe you want to come back later...\n"
-"    </body>\n"
-"</html>\n";
+	    "<html>\n"
+	    "    <head>\n"
+	    "	<title>503 Service Unavailable</title>\n"
+	    "    </head>\n"
+	    "   <body>\n"
+	    "	<h1>503 Service Temporarily Unavailable</h1>\n"
+	    "	    This HTTP service is unfortunately temporarily unavailable :(\n"
+	    "	    Maybe you want to come back later...\n"
+	    "    </body>\n"
+	    "</html>\n";
     } else
     {
 	std::ifstream user_content(ht_body_filename);
@@ -82,65 +82,65 @@ void setup_httpdata(void)
 
 void process_connection(inet_stream* clsock,std::ofstream* logfile)
 {
-	time_t epoch_time;
-	char* timebuf = new char[26];
+    time_t epoch_time;
+    char* timebuf = new char[26];
 
-	try {
-		{
-			epoch_time = time(0);
-
-			ctime_r(&epoch_time,timebuf);
-
-			timebuf[24] = 0;
-
-			std::unique_lock<std::mutex> log_out(logfile_mutex);
-
-			*logfile << timebuf << " Client: " << clsock->gethost().c_str() << ":" << clsock->getport().c_str() << "\n";
-			logfile->flush();
-		}
-
-		*clsock << http_output;
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(300)); // apparently necessary for correct delivery
-
-		delete clsock;
-	} catch (socket_exception exc)
+    try {
 	{
-		std::cerr << exc.mesg;
-		delete[] timebuf;
-		return;
+	    epoch_time = time(0);
+
+	    ctime_r(&epoch_time,timebuf);
+
+	    timebuf[24] = 0;
+
+	    std::unique_lock<std::mutex> log_out(logfile_mutex);
+
+	    *logfile << timebuf << " Client: " << clsock->gethost().c_str() << ":" << clsock->getport().c_str() << "\n";
+	    logfile->flush();
 	}
 
-	delete[] timebuf;
+	*clsock << http_output;
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(300)); // apparently necessary for correct delivery
+
+	delete clsock;
+    } catch (socket_exception exc)
+    {
+	std::cerr << exc.mesg;
+	delete clsock;
+	return;
+    }
+
+    delete[] timebuf;
 }
 
 void accept_new_connections(inet_stream_server& srvsock)
 {
-	inet_stream* clsock;
+    inet_stream* clsock;
 
-	std::ofstream logfile(logfilename,std::ios_base::out|std::ios_base::app);
+    std::ofstream logfile(logfilename,std::ios_base::out|std::ios_base::app);
 
-	if ( ! logfile.good() )
+    if ( ! logfile.good() )
+    {
+	std::ofstream errorfile("503ERR",std::ios_base::out|std::ios_base::app);
+	errorfile << "Could not open logfile " << logfilename << " for writing! Exited without doing anything\n";
+	errorfile.close();
+	exit(1);
+    }
+
+    while ( 1 )
+    {
+	try {
+	    clsock = srvsock.accept();
+
+	    std::thread worker(process_connection,clsock,&logfile);
+	    worker.detach();
+
+	} catch (socket_exception exc)
 	{
-	    std::ofstream errorfile("503ERR",std::ios_base::out|std::ios_base::app);
-	    errorfile << "Could not open logfile " << logfilename << " for writing! Exited without doing anything\n";
-	    errorfile.close();
-	    exit(1);
+	    std::cerr << exc.mesg;
 	}
-
-	while ( 1 )
-	{
-		try {
-			clsock = srvsock.accept();
-
-			std::thread worker(process_connection,clsock,&logfile);
-			worker.detach();
-
-		} catch (socket_exception exc)
-		{
-			std::cerr << exc.mesg;
-		}
-	}
+    }
 }
 
 int main(int argc, char** argv)
@@ -164,26 +164,26 @@ int main(int argc, char** argv)
 
     setup_httpdata();
 
-	try {
-		inet_stream_server srvsock("10.0.2.78",argv[optind],LIBSOCKET_IPv4); // If we bind to v6 ::, we get IPv4 connections too on most Linux systems (net.ipv6.bindv6only)
+    try {
+	inet_stream_server srvsock("::",argv[optind],LIBSOCKET_IPv6); // If we bind to v6 ::, we get IPv4 connections too on most Linux systems (net.ipv6.bindv6only)
 
-		close(0);
-		close(1);
-		close(2);
+	close(0);
+	close(1);
+	close(2);
 
-		pid = fork();
+	pid = fork();
 
-		if ( pid != 0 )
-		    exit(0);
+	if ( pid != 0 )
+	    exit(0);
 
-		accept_new_connections(srvsock);
+	accept_new_connections(srvsock);
 
-		srvsock.destroy();
+	srvsock.destroy();
 
-	} catch (socket_exception exc)
-	{
-		std::cerr << exc.mesg;
-	}
+    } catch (socket_exception exc)
+    {
+	std::cerr << exc.mesg;
+    }
 
-	return 0;
+    return 0;
 }
